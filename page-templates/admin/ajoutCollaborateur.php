adf20ec434de98db10c756a65e17aea6664c7c58
SoniaAndrianantenaina <andrianantenainasonia@gmail.com>
2023-09-13 16:40:01 +0300

    ajout nouveau collaborateur background image

--- page-templates/admin/ajoutCollaborateur.php
+++ page-templates/admin/ajoutCollaborateur.php
@@ -6,19 +6,23 @@
 
     <section class='block-ajoutCol'>
 
-        <figure class='block-ajoutCol__bg-img'>
+        <figure class='block-accueil__bg-img'>
             <img src='../../assets/images/collaborateur/cohésion.jpg' alt=''>
         </figure>
 
-        <div class='block-ajoutCol paragraphe'>
+        <div class='block-ajoutCol__paragraphe'>
             <div class='block-ajoutCol__text'>
-                <h2 class='block-ajoutCol__subtitle'>Nouveau Collaborateur</h2>
-                <h3 class='block-ajoutCol__cause'>
-                    Au sein d’une entreprise la gestion des Ressources humaines couvre de multiples aspects tous très importants pour le bon
-                    fonctionnement de la société.
-                </h3>
+                <h2 class='block-ajoutCol subtitle-collab'>Nouveau Collaborateur</h2>
+               
+                    <h3 class='block-ajoutCol cause-collab'>
+                        La meilleure des façons de commencer, c'est de prévenir les équipes internes qu'un
+                        <strong>nouveau collaborateur</strong> va rejoindre l'entreprise.
+                    </h3>
+              
             </div>
         </div>
 
     </section>
+
+    hello
 </main>
