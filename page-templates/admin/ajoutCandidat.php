5d9bebb8b0b27a02d3a1d6a5a172fe2ae89a1c91
SoniaAndrianantenaina <andrianantenainasonia@gmail.com>
2023-09-14 16:57:26 +0300

    page ajout candidat

---
+++ page-templates/admin/ajoutCandidat.php
@@ -0,0 +1,106 @@
+<?php include '../banner/header.php';
+
+?>
+
+<main class='page-ajout-candidat'>
+
+    <section class='block-ajoutCol'>
+
+        <figure class='block-accueil__bg-img'>
+            <img src='../../assets/images/collaborateur/recrutement.jpg' alt=''>
+        </figure>
+
+        <div class='block-ajoutCol__paragraphe'>
+            <div class='block-ajoutCol__text'>
+                <h2 class='block-ajoutCol subtitle-collab'>Nouveau Candidat</h2>
+
+                <h3 class='block-ajoutCol cause-collab'>
+                    Le recrutement est un <strong>facteur d'accroissement économique</strong> : il permet d'augmenter le chiffre d'affaires, 
+                    <strong>parfaire les marges bénéficiaires</strong>, dégager du temps susceptible d'être alloué à d'autres tâches
+                    au sein de l'entreprise, et <strong>apporter de l'aide aux apprenants qualifiés.</strong> 
+                </h3>
+
+            </div>
+        </div>
+
+    </section>
+
+    <section class="formulaire">
+        <div class="formulaire-collab">
+            <div class="block-annonces__item__title">
+                <h2 class='title-h2'>VEUILLEZ REMPLIR LE FORMULAIRE SUIVANT :</h2>
+            </div>
+
+            <div class="formulaire-collab__content">
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Nom">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Prénom">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="date" class="btn-blue" name="" id="" placeholder="Date de naissance">
+                </div>
+                
+                <div class="select">
+                    <select name="" id="">
+                        <option value="">Genre</option>
+                        <option value="">Homme</option>
+                        <option value="">Femme</option>
+                    </select>
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Contact">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Adresse">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Statut Marital">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="email" class="btn-blue" name="" id="" placeholder="E-mail">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Numéro CIN">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="number" class="btn-blue" name="" id="" placeholder="Nombre enfants">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="file" class="btn-blue" name="" id="" placeholder="Photo">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="CV">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Département">
+                </div>
+
+                <div class="formulaire-collab__content input">
+                    <input type="text" class="btn-blue" name="" id="" placeholder="Poste">
+                </div>
+
+
+            </div>
+
+                <div class="btn-générer">
+                    <a href="envoyerIdentifiant.php" class="btn-blue primary medium">Valider</a>
+                </div>
+
+        </div>
+    </section>
+
+</main>
