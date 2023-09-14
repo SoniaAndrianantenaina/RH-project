2fcd8f71632986adb2cd01ce9529ffc804ed1c78
SoniaAndrianantenaina <andrianantenainasonia@gmail.com>
2023-09-14 14:54:06 +0300

    anniversaire collaborateur

---
+++ page-templates/admin/annivCollaborateur.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../../css/index.css">
+    <title>Joyeux anniversaire</title>
+</head>
+<body class="grey">
+    <main class="anniversaire-collaborateur">
+        <div class="anniversaire">
+            <div class="anniversaire__left">
+                <figure class='anniversaire__left-image'>
+                    <img src='../../assets/images/anniv/gâteau.jpg' alt=''>
+                </figure>
+            </div>
+
+            <div class="anniversaire__right">
+                <div class="anniversaire__right title">
+                    <h3>JOYEUX ANNIVERSAIRE</h3>
+                </div>
+
+                <div class="anniversaire__right paragraphe">
+                    <p>
+                    En ce 21 Septembre 2023, nous tenons à vous souhaiter un très joyeux anniversaire ANDRIANANTENAINA Sonia! 🎉 <br>
+                    C'est le moment idéal pour célébrer non seulement une année de plus de réalisations et d'expérience, mais aussi 
+                    pour se réjouir des nouvelles opportunités à venir. Votre dévouement, votre expertise et votre passion pour votre
+                    travail sont des atouts inestimables pour notre équipe et notre entreprise.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+</html>
